a27e66300386139873b368caa9d36e28ff2658c5
Sefa Keskin <keskinsefa08@gmail.com>
2026-05-02 18:45:14 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.00%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_stack=[]
+        parantheses=list(s)
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for i in parantheses:
+
+            if my_stack and i in mapping and my_stack[-1]==mapping[i]:
+                my_stack.pop()
+            else:
+                my_stack.append(i)
+        
+        if len(my_stack)==0:
+            return True
+        else:
+            return False
+        
